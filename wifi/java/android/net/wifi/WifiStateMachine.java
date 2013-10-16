df7f4ccb926e55d85d6d12a0133ed5ddd9c0be3c
Kasper Hettinga <kasper.hettinga@gmail.com>
2013-10-16 19:08:13 +0200

    Revert "P2P: Do not restart SAP/P2P Go if its operating in 5G."
    This reverts commit 39cef5af43e2fe0ee110988abbce7ced1d2a4b1c.

--- wifi/java/android/net/wifi/WifiStateMachine.java
+++ wifi/java/android/net/wifi/WifiStateMachine.java
@@ -744,12 +744,8 @@ public class WifiStateMachine extends StateMachine {
                          if (currentChannel >= 0 &&
                             (currentChannel < startSafeChannel ||
                              currentChannel > endSafeChannel)) {
-                             //currently RIL passes only 2.4G channels so if the current operating
-                             // channel is 5G channel, do not restart SAP.
-                             if (currentChannel >= 1 &&  currentChannel <=14) {
-                                 Log.e(TAG, "Operating on restricted channel! Restart SAP");
-                                 restartSoftApIfOn();
-                             }
+                             Log.e(TAG, "Operating on restricted channel! Restart SAP");
+                             restartSoftApIfOn();
                          }
                      }
                  }
